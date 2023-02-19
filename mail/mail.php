85877db35a73b27d15e9e1374f4a426ec4a08c2d
AlNah <53786438+Nawaraj1111@users.noreply.github.com>
2023-02-19 20:05:05 +0545

    Update mail.php
    Update code for mail in php

--- mail/mail.php
+++ mail/mail.php
@@ -1,17 +1,24 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$message = $_POST['message'];
+<?php
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$message = htmlspecialchars($_POST['message']);
 
 $email_message = "
-
 Name: ".$name."
 Email: ".$email."
 Message: ".$message."
-
 ";
 
-mail ("luitel.nawaraj12300@gmail.com" , "New Message", $email_message);
-header("location: ../mail-success.html ");
-?>
+$to = "luitel.nawaraj12300@gmail.com";
+$subject = "New Message";
+$headers = "From: " . $email . "\r\n" .
+           "Reply-To: " . $email . "\r\n" .
+           "X-Mailer: PHP/" . phpversion();
 
+if (mail($to, $subject, $email_message, $headers)) {
+   header("Location: ../mail-success.html");
+   exit();
+} else {
+   echo "There was a problem sending the email.";
+}
+?>
